f1288b54c4db7cce985a4ed94289bb8748a6bc0e
Jeremy <muhohoweb@icloud.com>
2025-04-19 12:12:11 +0300

    Show cards again
    Signed-off-by: Jeremy <muhohoweb@icloud.com>

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 //    ->name('dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
 Route::middleware(['auth'])->group(function () {
