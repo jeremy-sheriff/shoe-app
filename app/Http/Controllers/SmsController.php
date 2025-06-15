7833d492b8d03f03a710ba2255984243cb7e2859
Jeremy <muhohoweb@icloud.com>
2025-06-16 01:34:05 +0300

    Message params validations

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -1,24 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Classes\AfricasTalkingGateway;
 use App\Classes\AfricasTalkingGatewayException;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class SmsController
 {
-
-    /**
-     * @throws AfricasTalkingGatewayException
-     */
-    public function sendSms(): void
+    public function sendSms(Request $request): void
     {
+        // Validate incoming request
+        $validated = $request->validate([
+            'cardId' => 'required|string',
+            'phoneNumber' => 'required|regex:/^07\d{8}$/'
+        ]);
+
         // Core parameters
         $userId   = 'Pamatech';
         $password = env('SMS_PASSWORD');
-        $mobile   = '254712419949';
-        $message  = 'Hello World! This is a test message!';
+        $mobile   = '254' . substr($validated['phoneNumber'], 1); // Convert 07... to 2547...
+        $message  = "Card ID {$validated['cardId']} swiped.";
         $senderId = env('SMS_SENDER_ID');
 
         $curl = curl_init();
@@ -44,48 +45,12 @@ class SmsController
 
         curl_close($curl);
 
-        Log::info($response);
+        Log::info("SMS Response: " . $response);
 
         if ($err) {
-            echo "cURL Error #: " . $err;
+            abort(500, "SMS Send Error: $err");
         } else {
-            echo $response;
+            response()->json(json_decode($response), 200)->send();
         }
     }
-
-
-
-    public function sendSMS2(): void{
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://smsportal.hostpinnacle.co.ke/SMSApi/send",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "userid=Pamatech&password=ebuc7hZr&mobile=254712419949&msg=Hello+World%21+This+is+a+test+message%21&senderid=Pamatech&msgType=text&duplicatecheck=true&output=json&sendMethod=quick",
-            CURLOPT_HTTPHEADER => array(
-                "apikey: ". env('SMS_API_KEY'),
-                "cache-control: no-cache",
-                "content-type: application/x-www-form-urlencoded"
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        Log::info($response);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
-        }
-
-    }
-
 }
