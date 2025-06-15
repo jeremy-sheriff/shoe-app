86eef40844b8a61c188d8f43a2bbce9d713cb71d
Jeremy <muhohoweb@icloud.com>
2025-06-16 01:13:11 +0300

    parameterize send message

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -12,17 +12,47 @@ class SmsController
     /**
      * @throws AfricasTalkingGatewayException
      */
-    public function sendSMS(): void
+    public function sendSms(): void
     {
-        $username = "muhoho";
-        $apiKey = env('AFRICAS_TALKING_API_KEY');
-        $to = "+254743822683";
-        $message = "Hello from AT";
-        $at = new  AfricasTalkingGateway($username, $apiKey);
+        // Core parameters
+        $userId   = 'Pamatech';
+        $password = env('SMS_PASSWORD');
+        $mobile   = '254712419949';
+        $message  = 'Hello World! This is a test message!';
+        $senderId = env('SMS_SENDER_ID');
 
-        $at->sendMessage($to, $message);
+        $curl = curl_init();
 
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://smsportal.hostpinnacle.co.ke/SMSApi/send",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "userid={$userId}&password={$password}&mobile={$mobile}&msg=" . urlencode($message) . "&senderid={$senderId}&msgType=text&duplicatecheck=true&output=json&sendMethod=quick",
+            CURLOPT_HTTPHEADER => array(
+                "apikey: " . env('SMS_API_KEY'),
+                "cache-control: no-cache",
+                "content-type: application/x-www-form-urlencoded"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        Log::info($response);
+
+        if ($err) {
+            echo "cURL Error #: " . $err;
+        } else {
+            echo $response;
+        }
     }
+
 
 
     public function sendSMS2(): void{
