142c451924adc1b2a2c55830b4c1e3cb60356370
Jeremy <muhohoweb@icloud.com>
2025-07-01 19:57:19 +0300

    Remove overflow take 1
    Signed-off-by: Jeremy <muhohoweb@icloud.com>

--- resources/views/livewire/orders/show.blade.php
+++ resources/views/livewire/orders/show.blade.php
@@ -56,7 +56,7 @@
 
         <div class="grid  auto-rows-min gap-4 md:grid-cols-2">
             <div
-                class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                class="relative aspect-video  rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <div class="space-y-2 p-6">
                     <form action="{{ route('orders.updateStatus', $order->uuid) }}" method="POST"
                           class="mt-6 space-y-4">
