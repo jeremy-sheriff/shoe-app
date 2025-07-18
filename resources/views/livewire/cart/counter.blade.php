f62773af38b7f2d041e3afab10e67edfac329dab
Jeremy <muhohoweb@icloud.com>
2025-07-18 08:19:33 +0300

    UI and verbiage
    Signed-off-by: Jeremy <muhohoweb@icloud.com>

--- resources/views/livewire/cart/counter.blade.php
+++ resources/views/livewire/cart/counter.blade.php
@@ -1,3 +1,5 @@
 <div>
-    Cart <sup>{{$totalItems}}</sup>
+    Cart
+    <flux:icon name="shopping-cart" class="inline w-4 h-4 ml-1"/>
+    <sup>{{$totalItems}}</sup>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,9 @@
             @auth
                 <a href="{{ url('/dashboard') }}" class="btn btn-outline">Dashboard</a>
             @else
-                <a href="{{ route('login') }}" class="hover:text-primary">Login</a>
+                <a href="{{ route('login') }}" class="hover:text-primary">
+                    <flux:icon name="user" class="inline w-4 h-4 ml-1"/>
+                    Login</a>
                 {{--                @if (Route::has('register'))--}}
                 {{--                    <a href="{{ route('register') }}" class="btn btn-primary">Cart</a>--}}
                 {{--                @endif--}}
@@ -221,7 +223,7 @@
                         class="text-lg font-bold text-primary dark:text-white">KSh {{ number_format($product->price, 2) }}</span>
                     <a href="{{ route('item.show', $product->slug) }}"
                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    >View</a>
+                    >Add to cart</a>
                 </div>
             </div>
         @endforeach
