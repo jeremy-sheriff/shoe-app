53ac0c989792b94412ab906c9a4174b3f4b69e3d
Jeremy <muhohoweb@gmail.com>
2025-08-05 23:39:16 +0300

    Comment
    Signed-off-by: Jeremy <muhohoweb@gmail.com>

--- database/migrations/2025_04_25_185939_create_items_table.php
+++ database/migrations/2025_04_25_185939_create_items_table.php
@@ -19,6 +19,7 @@ return new class extends Migration {
             $table->string('price')->nullable();
             $table->string('quantity')->nullable();
             $table->timestamps();
+            //
         });
     }
 
